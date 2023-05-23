d3245ac8b23e3a0b82dd082d2c73283cd9d3692c
Code <91371128+Ahmedxx7@users.noreply.github.com>
2023-05-23 17:34:24 +0300

    Add files via upload

---
+++ goHacking.sh
@@ -0,0 +1,312 @@
+#!/bin/bash
+a="\033[1;31m"
+b="\033[1;32m"
+d="\033[1;33m"
+c="\033[1;34m"
+
+clear
+                figlet -f big " Go Hacking " | lolcat
+                echo "         ############################################################"
+                echo -e $d "        ##"  "  Telegram" $a "https://t.me/x0f_3b                        ##"
+                echo "         ##                                                        ##"
+                echo -e $c "        ##"   "   github" $b "https://github.com/Ahmedxx7/goHack.git      ##"
+                echo "         ############################################################"
+
+
+
+
+
+
+
+
+
+                  echo -e $a"       1-Install top 10 tools"
+
+sleep .50
+                   echo -e  "        2-Tools Spam"
+
+sleep .50
+                   echo -e  "        3-Tools Phishing"
+
+sleep 1
+                   echo -e  "        4-Hack Accounts"
+
+sleep 1
+
+                   echo -e  "        5-Install MetaSploiT And Ngrok"
+
+sleep 1
+                   echo -e  "        6-Other"
+
+sleep 1
+                   echo -e   "       00-ExiT"
+
+                   echo -e "$c ENTER NUMBER TOOL ➪"
+read sex
+if [ $sex = 1 ]
+then
+clear
+figlet -f big " Top Tools " | lolcat
+echo -e "$c     1-Apktool"
+sleep 1
+echo -e "       2-java"
+sleep 1
+echo -e "       3-metasploit"
+sleep 1
+echo -e "       4-sqlmap"
+sleep 1
+echo -e "       5-wifite"
+sleep 1
+echo -e "       6-beef"
+sleep 1
+echo -e "       7-routersploit"
+sleep 1
+echo -e "       8-Easy_Hack"
+sleep 1
+echo -e "       9-Optiva-FrameWork"
+sleep 1
+echo -e "       10-Litespam"
+sleep 1
+echo -e "      00-Back"
+sleep 1
+echo -e "$c ENTER NUMBER TOOL ➪"
+read knmn
+fi
+if [ $sex = 2 ] 
+then
+clear
+figlet -f big " Spam " | lolcat
+echo -e "$d    1-Spammer-Grab"
+sleep 1
+echo -e "      2-LItespam"
+sleep 1
+echo -e "      3-Email-Boomber"
+sleep 1
+echo -e "      4-spamscope"
+sleep 1
+echo -e "      5-Spammer-Email"
+sleep 1
+echo -e "      6-Egyptian-sms-spammer"
+sleep 1
+echo -e "      7-Spammer-bot"
+sleep 1
+echo -e "      8-dj-Spammer"
+sleep 1
+echo -e  "      00-Back"
+sleep 1
+echo -e "$c ENTER NUMBER TOOL ➪"
+read knmn2
+fi
+if [ $sex = 3 ]
+then
+clear
+figlet -f big " Phishing " | lolcat 
+echo -e "$b   1-Fb_phishng"
+sleep 1
+echo -e "     2-shellphish"
+sleep 1
+echo -e "     3-weeman"
+sleep 1
+echo -e "     4-socialfish"
+sleep 1
+echo -e  "      00-Back"
+sleep 1
+echo -e "$c ENTER NUMBER TOOL ➪"
+read knmn3
+fi
+if [ $sex = 4 ]
+then
+clear
+figlet -f big " Phishing " | lolcat 
+echo -e "       1-cupp"
+sleep 1
+echo -e "       2-facebook-brute-force"
+sleep 1
+echo -e "       3-fb.py"
+sleep 1
+echo -e "       4-Facebook.pl"
+sleep 1
+echo -e "       5-Probadle-WordList"
+sleep 1
+echo -e "       6-hydra"
+sleep 1
+echo -e "       7-Hack-Yahoo"
+sleep 1
+echo -e  "      00-Back"
+sleep 1
+echo -e "$c ENTER NUMBER TOOL ➪"
+sleep 1
+read knmn4
+fi
+if [ $sex = 5 ]
+then
+figlet -f big " Meta && NGROK " | lolcat 
+clear
+echo -e "$d    1-MetaSploiT"
+sleep 1
+echo -e "$c    2-Ngrok"
+sleep 1
+echo -e  "      00-Back"
+sleep 1
+echo -e "$c ENTER NUMBER TOOL ➪"
+read knmn5
+fi
+if [ $sex = 6 ]
+then
+clear
+figlet -f big " Other " | lolcat 
+echo -e  "$a    1-Install Ubuntu"
+sleep 1
+echo -e "      2-Install Fedora"
+sleep 1
+echo -e "      3-Install NetHunter"
+sleep 1
+echo -e "      4-tor"
+sleep 1
+echo -e "      5-King-Hacking"
+sleep 1
+echo -e  "      6-setoolkit"
+sleep 1
+echo -e "      7-Tmux-Bunch"
+sleep 1
+echo -e "      8-fb_groub_hack"
+sleep 1
+echo -e "      9-Apkinjector"
+sleep 1
+echo -e  "      00-Back"
+sleep 1
+echo -e "$c ENTER NUMBER TOOL ➪"
+read knmn6
+fi
+if [ $sex = 00 ]
+then
+clear
+echo -e "Bye Bye :D"
+exit
+fi
+
+case $knmn in
+1) git clone https://github.com/Hax4us/apktool
+;;
+2) git clone https://github.com/Hax4us/java
+;;
+3) git clone https://github.com/rapid7/metasploit-framework
+;;
+4) git clone https://github.com/sqlmapproject/sqlmap
+;;
+5) git clone https://github.com/derv82/wifite
+;;
+6) git clone https://github.com/beefproject/beef
+;;
+7) git clone https://github.com/threat9/routersploit
+;;
+8) git clone https://github.com/sabri-zaki/EasY_HaCk
+;;
+9) git clone https://github.com/joker25000/Optiva-Framework
+;;
+10) git clone https://github.com/4L13199/LITESPAM
+;;
+esac
+case $knmn2 in
+1) git clone https://github.com/p4kl0nc4t/Spammer-Grab
+;;
+2) git clone https://github.com/4L13199/LITESPAM
+;;
+3) git clone https://github.com/zanyarjamal/Email-bomber
+;;
+4) git clone https://github.com/SpamScope/spamscope
+;;
+5) git clone https://github.com/p4kl0nc4t/Spammer-Email
+;;
+6) git clone https://github.com/WazeHell/Egyptian-SMS-Spammer
+;;
+7) git clone https://github.com/3pehrdev/spammer-bot
+;;
+8) git clone https://github.com/pydanny/dj-spam
+;;
+esac
+
+case $knmn3 in
+1) git clone https://github.com/SheehabX/FB-Phishing
+;;
+2) git clone https://github.com/shellphish
+;;
+3) git clone https://github.com/evait-security/weeman
+;;
+4) git clone https://github.com/UndeadSec/SocialFish
+;;
+esac
+case $knmn4 in
+1) git clone https://github.com/Mebus/cupp
+;;
+2) git clone https://github.com/HackerAdana/facebook-brute-force
+;;
+3) git clone https://github.com/emre/fb.py
+;;
+4) git clone https://github.com/bcattaneo/Facebook.pl
+;;
+5) git clone https://github.com/berzerk0/Probable-Wordlists
+;;
+6) pkg install hydra
+;;
+7) git clone https://github.com/kkruecke/hack-yahoo-finance-scrapper
+;;
+esac
+case $knmn5 in
+1) git clone https://github.com/rapid7/metasploit-framework
+;;
+2) echo " Please Open Url https://ngrok.com/docs And Download File Ngrok"
+echo " And Start Ngrok Using ./ngrok tcp YourPort"
+;;
+esac
+case $knmn6 in
+1) git clone https://github.com/Neo-Oli/termux-ubuntun
+;;
+2) https://github.com/nmilosev/termux-fedora
+;;
+3) git clone https://github.com/Hax4us/Nethunter-In-Termux
+;;
+4) pkg install tor
+;;
+5) git clone https://github.com/king-hacking/King-Hacking
+;;
+6) git clone https://github.com/Hax4us/setoolkit
+;;
+7) git clone https://github.com/Hax4us/Tmux-Bunch
+;;
+8) git clone https://github.com/Mr-NemrMedo/fb-group-hack
+;;
+9) git clone https://github.com/jbreed/apkinjector
+;;
+esac
+if [ $knmn = "00" ]
+then
+clear
+bash goHacking.sh 
+fi
+if [ $knmn2 = "00" ]
+then
+clear
+bash goHacking.sh 
+fi
+if [ $knmn3 = "00" ]
+then
+clear
+bash goHacking.sh 
+fi
+if [ $knmn4 = "00" ]
+then
+clear
+bash goHacking.sh 
+fi
+if [ $knmn5 = "00" ]
+then
+clear
+bash goHacking.sh 
+fi
+if [ $knmn6 = "00" ]
+then
+clear
+bash goHacking.sh 
+fi
+
---
+++ update.sh
@@ -0,0 +1,15 @@
+clear
+d="\033[1;33m"
+
+
+figlet -f big " Update Tool " | lolcat
+
+sleep 1
+echo -e $d "[*] install update "
+sleep 2
+cd ~ 
+rm -rf goHack
+git clone https://github.com/Ahmedxx7/goHack.git 
+
+echo -e $d "to update ♡"
+
